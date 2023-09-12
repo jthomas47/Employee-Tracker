67989f0cf197a5ca78b68efdbcfe84208fe9e8ed
jthomas47 <jackspurthomas@gmail.com>
2023-09-12 03:34:03 -0500

    created init() to start the appliction

--- index.js
+++ index.js
@@ -9,3 +9,38 @@ db.connect((err) => {
         throw err;
     }
 });
+
+// prompt user 
+function promptUser () {
+    return inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'userInput',
+                message: 'What would you like to do?',
+                choices: [
+                    'View all departments',
+                    'View all roles',
+                    'view all employees',
+                    'Add a department',
+                    'Add a role',
+                    'Add an employee',
+                    'Update current employee role',
+                    'Quit'
+                ]
+            }
+        ]);
+}
+
+
+// starts the application
+function init() {
+    promptUser()
+        .then((answers) => {
+            handleInput(answers); 
+        });
+}
+
+
+init(); 
+
